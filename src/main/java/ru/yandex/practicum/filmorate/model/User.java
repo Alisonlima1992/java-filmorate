6772289a885c8db288c19dca12752aa1bc36cc8c
Alisonlima <aurin7128@gmail.com>
2025-06-22 03:39:29 +0700

    Initial commit

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -14,4 +16,6 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Long> friends = new HashSet<>();
+
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.Exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.*;
+
+@Component
+@Slf4j
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextId = 1;
+
+    @Override
+    public User create(User user) {
+        user.setId((int)nextId++);
+        users.put(user.getId().longValue(), user);
+        log.info("Пользователь успешно добавлен: {}", user);
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        if (!users.containsKey(user.getId().longValue())) {
+            log.warn("Пользователь с id {} не найден для обновления.", user.getId());
+            throw new NotFoundException("Пользователь с id " + user.getId() + " не найден.");
+        }
+        users.put(user.getId().longValue(), user);
+        log.info("Пользователь успешно обновлен: {}", user);
+        return user;
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("Получен запрос на получение списка всех пользователей.");
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public Optional<User> getById(long id) {
+        log.info("Получен запрос на получение пользователя с id: {}", id);
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public void delete(long id) {
+        if (!users.containsKey(id)) {
+            log.warn("Пользователь с id {} не найден для удаления.", id);
+            throw new NotFoundException("Пользователь с id " + id + " не найден.");
+        }
+        users.remove(id);
+        log.info("Пользователь с id {} успешно удален.", id);
+    }
+}
